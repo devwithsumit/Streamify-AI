e5344b77b692f6a0f047719343b642ffefb77cce
webwithsumit <sumit2410kushwaha@gmail.com>
2025-04-11 11:11:11 +0530

    use movie detail hook

--- src/utils/useMovieDetail.js
+++ src/utils/useMovieDetail.js
@@ -1,19 +1,27 @@
 import { useEffect, useState } from "react";
 import axiosInstance from '../utils/axiosInstance';
 import { toast } from 'react-toastify';
+import { useSearchContext } from "../context/SearchContext";
 
-const useMovieDetails = (id)=>{
-    const [movieDets, setMovieDets] = useState({});
+const useMovieDetails = (id) => {
+    const [movieDets, setMovieDets] = useState(null);
+    const { setIsLoading } = useSearchContext();
+    
     useEffect(() => {
-        axiosInstance.get('https://api.themoviedb.org/3/movie/' + id)
-            .then((response) => {
-                console.log(response.data);
+        const fetchMovieDetail = async () => {
+            setIsLoading(true);
+            try {
+                const response = await axiosInstance.get('https://api.themoviedb.org/3/movie/' + id)
+                // console.log(response)
                 setMovieDets(response.data);
-            })
-            .catch((error) => {
+            } catch (error) {
                 toast.error("Error fetching movie details")
                 console.log(error);
-            })
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchMovieDetail();
     }, [id])
     return movieDets;
 }
