3bbdd42ccda14a73c3c9d1abdc519bef9543e692
webwithsumit <sumit2410kushwaha@gmail.com>
2025-04-15 11:11:11 +0530

    home and sign in page fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
-import React, { useEffect } from 'react'
-import SignIn from './SignIn'
+import MainContainer from '../components/home/MainContainer'
+import HeroSection from '../components/home/HeroSection';
 
 const Home = () => {
-
   return (
-    <div className={`min-h-[100vh] **:not-[]:relative pt-28 global-px bg-center bg-cover bg-[url(https://raw.githubusercontent.com/thatanjan/netflix-clone-yt/youtube/media/banner.jpg)]`}>
-      <div className="div absolute z-0 w-full h-full bg-gradient-to-r from-black/88 via-black/75 to-black/1 left-0 top-0"></div>
-      <div className="w-full relative">
-        {/* Home */}
-        <SignIn />
+    <div className={`block min-h-[100vh] relative`}>
+      <HeroSection />
+      <div className="w-full global-px relative pt-10 bg-gradient-to-b from-white dark:from-black">
+        <MainContainer />
       </div>
     </div>
 
