745f4353f296c5b5edbd21d823b74d51ca7d9227
webwithsumit <sumit2410kushwaha@gmail.com>
2025-04-26 11:11:11 +0530

    Update drop down and gemini logo

---
+++ src/components/user/UserDropDown.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const UserDropDown = ({user, handleSignOut}) => {
+  return (
+      <div id="user-dropdown" className="z-[100] hidden absolute my-4 top-8 -right-5 sm:-right-2 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600"
+          data-popper-placement="bottom">
+          <div className="px-4 py-3">
+              <span className="block text-sm text-gray-900 dark:text-white">{user?.displayName || "Bonnie Green"}</span>
+              <span className="block text-sm text-wrap text-gray-500 truncate dark:text-gray-400">{user?.email.slice(0, 15).concat("...") || "name@flowbite.com".concat("...")}</span>
+          </div>
+          <ul onClick={() => document.getElementById("user-dropdown").classList.add("hidden")}
+              className="py-2" aria-labelledby="user-menu-button">
+              <li>
+                  <Link to='/home' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                      Dashboard
+                  </Link>
+              </li>
+              <li>
+                  <Link to='/update-profile' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                      Settings
+                  </Link>
+              </li>
+              <li>
+                  <button onClick={handleSignOut} className="w-full text-start block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                      Sign out
+                  </button>
+              </li>
+          </ul>
+      </div>
+  )
+}
+
+export default UserDropDown
