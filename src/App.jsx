dfab878d95d3544f53a1852bb3968064c683630e
webwithsumit <sumit2410kushwaha@gmail.com>
2025-05-01 11:11:11 +0530

    Browser router Context and Store Configure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Navbar from './components/main/Navbar'
+import Footer from './components/main/Footer'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 
@@ -15,11 +15,16 @@ import OfflinePage from './pages/OfflinePage'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Services from './pages/Services'
+import { ToastContainer } from 'react-toastify'
+import UpdateProfile from './components/user/UpdateProfile'
+import MoviePage from './pages/MoviePage'
+import { useSearchContext } from "./context/SearchContext"
 
 function App() {
   const online = useOnlineStatus();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const {setIsSearchPage} = useSearchContext();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -28,36 +33,42 @@ function App() {
         // https://firebase.google.com/docs/reference/js/auth.user
         const uid = user.uid;
         dispatch(addUser(getSerializableUser(user)));
-        navigate("/");
+        navigate("/home");
       } else {
         // User is signed out
         dispatch(removeUser());
         navigate("/login");
       }
     });
-    return ()=>{
+    return () => {
       // unsubscribe when component unmounts
       unsubscribe();
     }
   }, [])
 
+
   if (!online) {
     return <OfflinePage />
   }
+
   return (
-    <div id='App' className='dark:bg-[#161515] min-h-screen flex flex-col justify-start dark:text-white'>
+    <div id='App' className='dark:bg-[#161515] bg-gray-200 text-black min-h-screen flex flex-col justify-start dark:text-white'>
+      <ToastContainer />
       <Navbar />
-      <div className="mb-auto">
+      <div className="mb-auto" onClick={() => setIsSearchPage(false)}>
         <Routes>
-          <Route path='/' element={<Home />} >
-            <Route path='/login' element={<SignIn />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/services' element={<Services />} />
-          </Route>
+          <Route path='/update-profile' element={<UpdateProfile />} />
+          <Route path='/login' element={<SignIn />} />
+          <Route path='/home' element={<Home />}/>
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/movie/:id' element={<MoviePage />}/>
         </Routes>
       </div>
-      <Footer />
+      <div onClick={() => setIsSearchPage(false)}>
+        <Footer />
+      </div>
     </div>
   )
 }
