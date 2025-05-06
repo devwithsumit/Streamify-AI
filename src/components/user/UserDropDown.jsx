13be77b485ee0b3149d30117acf93e2b938f4232
webwithsumit <sumit2410kushwaha@gmail.com>
2025-05-06 11:11:11 +0530

    flowbite error fix

--- src/components/user/UserDropDown.jsx
+++ src/components/user/UserDropDown.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 const UserDropDown = ({user, handleSignOut}) => {
   return (
-      <div id="user-dropdown" className="z-[100] hidden my-4 -right-12 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600"
+      <div id="user-dropdown" className="z-[100] hidden my-4 absolute top-8 -right-2 sm:-right-2 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600"
           data-popper-placement="bottom">
           <div className="px-4 py-3">
               <span className="block text-sm text-gray-900 dark:text-white">{user?.displayName || "Bonnie Green"}</span>
               <span className="block text-sm text-wrap text-gray-500 truncate dark:text-gray-400">{user?.email.slice(0, 15).concat("...") || "name@flowbite.com".concat("...")}</span>
           </div>
-          <ul
+          <ul onClick={() => document.getElementById("user-dropdown").classList.add("hidden")}
               className="py-2" aria-labelledby="user-menu-button">
               <li>
                   <Link to='/home' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
